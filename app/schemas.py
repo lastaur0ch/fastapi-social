8c60ba34e2f077f9af5fd440cb86acb74c8d7650
lastaur0ch <joelxjohnson@gmail.com>
2023-09-22 22:03:04 +0100

    rectified schema inheritance

--- app/schemas.py
+++ app/schemas.py
@@ -10,12 +10,10 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
-#define the response model data. Can inherit from PostBase and present only additional fields
-class Post(BaseModel):
+#define the response model data. Can inherit from BaseModel and specify all fields to be sent to FE
+#here I inherited PostBase so all its properties must the shown and the additional ones I stated
+class Post(PostBase):
     id: int
-    title: str
-    content: str
-    published: bool
     created_at: datetime
 
     class Config:
