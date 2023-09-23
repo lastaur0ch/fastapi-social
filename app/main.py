886bfb16cd745574b110882c5b5cf6ae3e5821f9
lastaur0ch <joelxjohnson@gmail.com>
2023-09-23 06:44:52 +0100

    password hashing in utils file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import Optional, List
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -63,8 +64,21 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    #hash password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"User with ID: {id} does not exist. Try a different ID")
+    return user
