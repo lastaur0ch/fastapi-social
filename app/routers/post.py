1fac08f008398f3224ae22cf112323a9c8c12a0c
lastaur0ch <joelxjohnson@gmail.com>
2023-09-24 11:27:52 +0100

    implemented routers

---
+++ app/routers/post.py
@@ -0,0 +1,55 @@
+from fastapi import Body, FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing import List
+from .. import models, schemas
+from ..database import get_db
+
+
+router = APIRouter()
+
+
+@router.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump()) #maps the post parameters to the model columns
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post) #works like RETURNING *, it stores & returns the entire row/values into the new_post
+    return new_post
+
+
+@router.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with ID: {id} was not found. Try a different ID")
+    return post
+
+
+@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f'Post with id: {id} does not exist')
+    
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f'Post with id: {id} does not exist')
+    
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
